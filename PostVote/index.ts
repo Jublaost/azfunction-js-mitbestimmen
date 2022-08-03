3e22725cde13cb8e51924192eceaf33b328fc264
Thomas <65050898+tvonment@users.noreply.github.com>
2022-08-03 15:53:06 +0000

    add validation

--- PostVote/index.ts
+++ PostVote/index.ts
@@ -1,11 +1,24 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import axios, { AxiosRequestConfig } from 'axios';
+import qs = require('qs');
+
+const { v4: uuidv4 } = require('uuid')
+
 const RECAPTCHA = process.env["recaptchaCode"]
+const APP_ID = process.env["appId"];
+const APP_SECRET = process.env["appSecret"];
+const TENANT_ID = process.env["tenantId"];
+
+const TOKEN_ENDPOINT = 'https://login.microsoftonline.com/' + TENANT_ID + '/oauth2/v2.0/token';
+const MS_GRAPH_SCOPE = 'https://graph.microsoft.com/.default';
+const MS_GRAPH_ENDPOINT_SENDMAIL = 'https://graph.microsoft.com/v1.0/users/mitbestimmen@jublaost.ch/sendMail';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, voteIn): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     context.log("Body: ", req.body)
     context.log("VoteIn: ", voteIn)
+
+    axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
     let validation = await validateRECAP(context, req.body["g-recaptcha-response"]);
 
@@ -26,11 +39,15 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         return
     }
 
+    let uuid = uuidv4();
     let vote = req.body;
+    vote.code = uuid;
     context.log("Vote: ", vote)
 
     try {
         context.bindings.voteOut = vote;
+        let token = await getToken();
+        let mail = await sendMail(token, vote);
         context.res = {
             status: 200,
             body: "successful"
@@ -63,5 +80,65 @@ async function validateRECAP(context: Context, token: string) {
         })
         .catch(error => {
             context.log(error);
+        });
+}
+
+
+/**
+ * Get Token for MS Graph
+ */
+async function getToken(): Promise<string> {
+    const postData = {
+        client_id: APP_ID,
+        scope: MS_GRAPH_SCOPE,
+        client_secret: APP_SECRET,
+        grant_type: 'client_credentials'
+    };
+
+    return await axios
+        .post(TOKEN_ENDPOINT, qs.stringify(postData))
+        .then(response => {
+            // console.log(response.data);
+            return response.data.access_token;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+
+async function sendMail(token: string, vote: any) {
+    let config: AxiosRequestConfig = {
+        method: 'post',
+        url: MS_GRAPH_ENDPOINT_SENDMAIL,
+        headers: {
+            'Authorization': 'Bearer ' + token //the token is a variable which holds the token
+        },
+        data: {
+            "message": {
+                "subject": "Verifizierung und Abschluss des Votings!",
+                "body": {
+                    "contentType": "html",
+                    "content": "Vielen Dank füre deine Stimme!<br />'" + vote.category + "' mit der Option '" + vote.option + "'<br />Bitte bestätige nur noch deine Stimme mit folgendem Link: <a href='https://web-mitbestimmen.azurewebsites.net/api/VoteValidation?email=" + vote.email + "&code=" + vote.code + "'>Bestätigen</a><br /><br />Jublastische Grüsse<br />"
+                },
+                "toRecipients": [
+                    {
+                        "emailAddress": {
+                            "address": vote.email
+                        }
+                    }
+                ]
+            },
+            "saveToSentItems": "true"
+        }
+    }
+
+    return await axios(config)
+        .then(response => {
+            console.log(response.data);
+            return response.data.value;
+        })
+        .catch(error => {
+            console.log(error);
         });
 }
